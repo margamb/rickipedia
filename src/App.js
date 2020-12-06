0c1d262e7021af4a95d07b0d50498226238166d1
margamb <margamarben@gmail.com>
2020-12-06 10:28:58 +0100

    Add basic logic for fetching characters and location Still need to move the API to a different module.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { useEffect, useState } from 'react';
+
+const charachtersUrl = 'https://rickandmortyapi.com/api/character';
+const locationsUrl = 'https://rickandmortyapi.com/api/location';
+
+const rickMortyAPI = {
+  getCharacters: async function fetchCharacters() {
+    const res = await fetch(charachtersUrl);
+    const data = await res.json();
+    return data.results;
+    // return fetch(charachtersUrl).then((res) => res.json());
+  },
+
+  getLocation: async function fetchCharacters() {
+    const res = await fetch(locationsUrl);
+    const data = await res.json();
+    return data.results;
+  },
+};
+
 function App() {
-  return <div>Hola</div>;
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function fetchCharacters() {
+      // Get characters data from API
+      const characters = await rickMortyAPI.getCharacters();
+      // Set character state to that result
+      setCharacters(characters);
+    }
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div>
+      <input type="text" />
+      {characters.map((character) => (
+        <div key={character.name}>
+          <h1>{character.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
